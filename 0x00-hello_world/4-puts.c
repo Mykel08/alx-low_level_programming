8ab901f3fd5144b1b47b50aa682abbc5b602c636
Mykel08 <mykelinfocon@gmail.com>
2022-12-08 17:40:08 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-
+i#include <stdio.h>
 /**
- *
- * *main - Entry point
- *
- * *Return: Always 0 (Success)
- *
- * */
-
-
-
+ * main - main block
+ * return: 0
+ * /
 int main(void)
-
 {
+	puts('\"programming is like building a multilingual puzzle");
+	return (0);
+}
