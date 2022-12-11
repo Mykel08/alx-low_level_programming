ab0b119ce72116def4e4fbffb89b56648d2b8564
Mykel08 <mykelinfocon@gmail.com>
2022-12-11 04:52:18 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *  main - Prints the last digit of a randomly generated umber and wether is greater than 5, less than 6, or 0 .
+ *  main - Prints the last digit of a  umber and wether is greater than 5, less than 6, or 0 .
  *  Return: Always 0 .
 */
 int main(void)
