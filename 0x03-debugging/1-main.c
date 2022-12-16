67612b4a0141e0c33e90a1c60bd81de70436fc3d
Mykel08 <mykelinfocon@gmail.com>
2022-12-15 17:48:34 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,20 +5,20 @@
  * Return: 0
 */
 
-
-
 int main(void)
-
 {
+	int i;
 
+	printf("Infinite loop incoming : (\n");
 
-        int i;
+	i = 0;
 
-        printf("Infinite loop incoming :(\n");
-        i = 0;
-        /*while (i < 10)*/
-        /*{*/								                /*	putchar(i);*/
-        /*}*/
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
