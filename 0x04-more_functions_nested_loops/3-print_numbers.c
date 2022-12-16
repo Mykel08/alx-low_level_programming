414cc619078dbe7f05875cf4c32f7a1b3fbdd74f
Mykel08 <mykelinfocon@gmail.com>
2022-12-15 19:33:49 -0800

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for digit (0 through 0)
+ * Return: Always 0
+*/
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar("\n");
+}
