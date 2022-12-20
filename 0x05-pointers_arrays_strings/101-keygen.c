a876769340286b197ed9265e80ee82708a2c370d
Mykel08 <mykelinfocon@gmail.com>
2022-12-20 05:15:04 -0800

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -19,7 +19,7 @@ int main(void)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
-		putchar(pass[i] = '0');
+		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
