74755ae06779cd0d159e0800583f7aab2a9845c5
Mykel08 <mykelinfocon@gmail.com>
2022-12-22 02:37:29 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,20 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: on success 1.
- * on error, -1 is retured, and error is set appropriately.
-*/
-int _putchar(char c)
+ * strcat - Concatenates the string pointed to by @src, including the x
+ * null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcat(char *dest, const char *src)
 {
-	return (write(1, &c, 1));
+	int index = 0, dest_len = 0;
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
