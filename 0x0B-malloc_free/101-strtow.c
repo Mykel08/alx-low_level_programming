83424d44409a88de029769f0eacc566789d2bbe0
Mykel08 <mykelinfocon@gmail.com>
2023-01-10 16:06:26 -0800

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -33,12 +33,16 @@ int count_word(char *s)
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int i, k =0, len = 0, words, c = 0, start, end;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
 	while (*(str + len))
 		len++;
 	words = count_word(str);
 	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
 
 	for (i = 0; i <= len; i++)
