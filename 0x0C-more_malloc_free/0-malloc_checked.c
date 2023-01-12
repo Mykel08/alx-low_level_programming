2a7df297b51c8648c6416d6a2909fe009589dda6
Mykel08 <mykelinfocon@gmail.com>
2023-01-12 12:55:08 -0800

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,11 +5,11 @@
  * @b: number of bytes to allocate
  * Return: a pointer to the allocated memory
 */
-void *malloc_checker(unsigned int b)
+void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	prt = malloc(b);
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
