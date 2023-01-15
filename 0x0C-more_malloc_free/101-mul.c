b8fc8d8e15b3420dac20a866650057a13fa23625
Mykel08 <mykelinfocon@gmail.com>
2023-01-14 20:13:05 -0800

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -21,7 +21,6 @@ int is_digit(char *s)
 	}
 	return (1);
 }
-
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
@@ -35,9 +34,9 @@ int _strlen(char *s)
 	{
 		i++;
 	}
+
 	return (i);
 }
-
 /**
  * errors - handles errors for main
 */
@@ -46,7 +45,6 @@ void errors(void)
 	printf("Error\n");
 	exit(98);
 }
-
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -59,20 +57,25 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
+
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
+	
 	if (!result)
 		return (1);
+
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
+
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
+
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
@@ -83,6 +86,7 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
+
 	for (i = 0; i < len -1; i++)
 	{
 		if (result[i])
@@ -90,6 +94,7 @@ int main(int argc, char *argv[])
 		if (a)
 			_putchar(result[i] + '0');
 	}
+
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
