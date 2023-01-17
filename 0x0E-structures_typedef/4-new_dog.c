1741d0804c2741c2ed4f5154843faf56b5fdcf50
Mykel08 <mykelinfocon@gmail.com>
2023-01-16 23:50:20 -0800

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -47,7 +47,7 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 /**
- * new_dog - crestes a new dog
+ * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -56,7 +56,7 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int len1. len2;
+	int len1, len2;
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
