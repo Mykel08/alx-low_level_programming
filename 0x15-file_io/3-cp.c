4e4a8355a13d71b93c9271c359aab71ef68bf2d6
mykel08 <mykelinfocon@gmail.com>
2023-02-07 19:00:31 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,7 +18,7 @@ char *create_buffer(char *file)
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 	return (buffer);
@@ -69,12 +69,19 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);				free(buffer);									exit(98);
-		}									w = write(to, buffer, r);
-	if (to == -1 || w == -1)							{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
-		free(buffer);									exit(99);								}
-	r = read(from, buffer, 1024);							to = open(argv[2], O_WRONLY | O_APPEND);
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+	w = write(to, buffer, r);
+	if (to == -1 || w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		exit(99);
+	}
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 	free(buffer);
 	close_file(from);
